7e1e4af3ce5ec1f522e9d9e4628c144405adfbfd
Matthijs Kralt <matthijs29@live.com>
2018-02-09 17:33:53 +0100

    feat: Add checkCollisions function

--- js/app.js
+++ js/app.js
@@ -9,6 +9,9 @@ class Enemy {
     // Initial y-pos is either 63 (1st stone row), 146 (2nd) or 229 (3rd)
     this.y = 63 + (83 * randomNr(3));
     this.speed = 150 + (170 * Math.random());
+    // Set width and height for collision detection
+    this.width = 97;
+    this.height = 66;
   }
   // Update the enemy's position
   // Parameter: dt, a time delta between ticks
@@ -24,20 +27,19 @@ class Enemy {
   }
 }
 
-// Return a random nr between 0 and 2, which will set the bug's initial y-value to one of the three possible rows randomly
-function randomNr(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
   constructor() {
     this.sprite = 'img/char-boy.png';
+    // Set initial X and Y positions, mutable one and one for reference when collision or water is reached
     this.initialX = 202;
     this.x = 202;
     this.initialY = 395;
     this.y = 395;
+    // Set width and height for collision detection
+    this.width = 66;
+    this.height = 75;
   }
   // Update x and y position
   update(dx = 0, dy = 0) {
@@ -75,10 +77,15 @@ class Player {
   }
 }
 
+// Return a random nr between 0 and 2, which will set the bug's initial y-value to one of the three possible rows randomly
+function randomNr(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 // Instantiate objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-allEnemies = [];
+const allEnemies = [];
 
 function renderEnemies() {
   const enemy = new Enemy();
@@ -88,20 +95,35 @@ function renderEnemies() {
     allEnemies.shift();
   }
 }
+setInterval(renderEnemies, 700 + (500 * Math.random()));
 
-setInterval(renderEnemies, 900 + (1000 * Math.random()));
-
+// Instantiate player obj
 const player = new Player();
 
-//TODO: make checkCollisions function
 function checkCollisions() {
+  allEnemies.forEach(enemy => {
+    // Because the picture has some whitespace at the top and side, the x and y pos of the actual visual picture needs to be set
+    const actualXposEnemy = enemy.x + 1;
+    const actualYposEnemy = enemy.y + 76;
+    const actualXposPlayer = player.x + 16;
+    const actualYposPlayer = player.y + 63;
 
+    // Check if enemy obj and player obj collide (syntax from https://blog.sklambert.com/html5-canvas-game-2d-collision-detection/)
+
+    if (actualXposEnemy < actualXposPlayer + player.width && actualXposEnemy + enemy.width > actualXposPlayer && actualYposEnemy < actualYposPlayer + player.height && actualYposEnemy + enemy.height > actualYposPlayer) {
+      // Set small timeout, so player does actually 'collide' for a short amount of time instead of teleporting to start immediately
+      setTimeout(() => {
+        player.x = player.initialX;
+        player.y = player.initialY;
+      }, 20);
+    }
+  });
 }
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+document.addEventListener('keydown', function(e) {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
