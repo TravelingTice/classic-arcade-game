54f5cc2d9aabb3381807eaa426fe3a8d08a053ab
Matthijs Kralt <matthijs29@live.com>
2018-02-09 13:14:35 +0100

    feat: Create enemy class and render 1 enemy upon game refresh on random pos and at random speed on canvas

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 // Enemies our player must avoid
 class Enemy {
   constructor() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'img/enemy-bug.png';
+    // Initial x-pos is fixed
+    this.x = -101;
+    // Initial y-pos is either 63 (1st stone row), 146 (2nd) or 229 (3rd)
+    this.y = 63 + (83 * randomNr(3));
+    this.speed = 100 + (170 * Math.random());
   }
-  // Update the enemy's position, required method for game
+  // Update the enemy's position
   // Parameter: dt, a time delta between ticks
   update(dt) {
+    this.x += this.speed * dt;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -21,15 +24,21 @@ class Enemy {
   }
 }
 
+// Return a random nr between 0 and 2, which will set the bug's initial y-value to one of the three possible rows randomly
+function randomNr(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
   constructor() {
     this.sprite = 'img/char-boy.png';
+
   }
 
-  update(dt) {
+  update() {
 
   }
 
@@ -45,8 +54,10 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const enemy = new Enemy();
+allEnemies = [];
+allEnemies.push(enemy);
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
